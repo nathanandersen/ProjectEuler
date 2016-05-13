f089af0f91c3f5b78596ed838ed5bc8635aa01c5
Nathan Andersen <nathan.andersens@gmail.com>
2016-05-13 00:02:31 -0400

    Improved 92.

--- ImprovementNeeded/p092.py
+++ ImprovementNeeded/p092.py
@@ -1,36 +1,35 @@
 # Problem 92
-global nums89
-nums89 = {89}
-global nums1
-nums1 = {1}
 
-# This is ok and correct, but slow.
+
+# Slow.
+
+#real	1m8.287s
+#user	1m6.965s
+#sys	0m0.787s
+
+
+nums89 = set([89])
+nums1 = set([1])
+
 
 def next_sq_digit_num(n):
     return sum(int(d)**2 for d in str(n))
 
-def add_to_c89(chain):
-    for c in chain:
-        nums89.add(c)
-
-def add_to_c1(chain):
-    for c in chain:
-        nums1.add(c)
-
 def find_chain(i):
-    chain = []
-    while(1):
+    chain = set()
+    while True:
         if i in nums89:
-            add_to_c89(chain)
-            return 1
+            nums89.update(chain)
+            return True
         elif i in nums1:
-            add_to_c1(chain)
-            return 0
+            nums1.update(chain)
+            return False
         else:
-            chain.append(i)
+            chain.add(i)
             i = next_sq_digit_num(i)
 
 for i in range(1,10000000):
     print(i)
     find_chain(i)
+
 print(len(nums89))
