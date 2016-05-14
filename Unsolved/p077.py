5da4c49343604c609c33c1856b1a8ad84bb2a78c
Nathan Andersen <nathan.andersens@gmail.com>
2016-05-14 12:06:24 -0400

    Some code organization. It seems that an equation-solver could solve all problems in the Equations folder.

--- Unsolved/p077.py
+++ Unsolved/p077.py
@@ -1,11 +1,25 @@
 # Problem 77
 primes = sieve(1000)
 
-def take_steps(n,largest):
-    total = 1
-    if n < 0:
-        return 0
-    if n == 2:
-        return 1
-    for i in primes:
-        # complete me according to p076
+# we will define a table, N
+# where N(x) is the number
+# of ways x can be written as
+# the sum of prev. primes
+
+# to calculate arbitrary N(i),
+# we must take a prime p, and then
+# look up the value N(i-p)
+
+# this is cool, and all, but how do
+# we guarantee uniqueness?
+
+# 2+5 looks a lot like 5+2...
+
+# 27+2 could look a lot like 24+5
+
+# we will define a helper table, where
+# V(x) is the set of tuples of coin collections
+
+# so we will iterate over everything in everything up
+# to x, make a new set of tuples,
+
