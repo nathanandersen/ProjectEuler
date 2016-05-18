d79fbad679eab2e87c5cb3094760659625f1824d
Nathan Andersen <nathan.andersens@gmail.com>
2016-05-18 14:03:10 -0400

    Organization.

---
+++ Unsolved/p357.py
@@ -0,0 +1,8 @@
+# Problem 357
+
+# Note that for this to be the case,
+# n cannot be divisible by any square number
+
+# 100 million cannot be brute-forced.
+# so these numbers must have some special property.
+
