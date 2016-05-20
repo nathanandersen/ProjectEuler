527842f85447edc68ede6b8f67e7131d38246e9b
Nathan Andersen <nathan.andersens@gmail.com>
2016-05-19 22:58:59 -0400

    A further intuition on problem 78.

--- Unsolved/p078.py
+++ Unsolved/p078.py
@@ -1,78 +1,11 @@
 # Problem 78
 
-# This smells like dynamic programming.
+# Given that the group on the end is of size n,
+# every group to its left must be at least of size n.
 
-# For every possible group, from 2 -> 3
+# Could I do this with 2-D dynamic programming?
 
-# we can either add a coin to extend the back group ( o )
-# or we can add it as its own group ( |o)
-
-# how can we detemine whether the o is unique? the |o is always unique.
-
-# if we extend with a (o), and the relationship between all the groups
-# is no longer in order (the last one is not the smallest), then we don't
-# count it.
-# how to check this?
-
-
-# Maybe I could implement this with sets of tuples.
-# for each tuple in the prev layer
-# last number += 1, and append 1.
-# then sort the tuple
-# and add it to the set.
-# (then, dispose of the previous set).
-
-def make_coin_extensions(tup):
-    if isinstance(tup,int):
-        single = (1,) + (tup,)
-        extended = (tup+1,)
-    else:
-        single = (1,) + tup
-        
-
-        
-prev = set((1,))
-cur = set()
-x = 1
-while True:
-#if True:
-    for coins in prev:
-        if isinstance(coins,int):
-            single = (1,) + (coins,)
-            extended = (coins+1,)
-        else:
-            single = (1,) + coins
-            extended = coins[:len(coins)-2]
-            if isinstance(extended,int):
-                extended = (extended,) + (coins[len(coins-1):]+1,)
-            else:
-                extended = extended + (coins[len(coins-1):]+1,)
-            print(extended)
-
-        cur.add(single)
-        cur.add(extended)
-    if not len(cur) % 1000000:
-        print(x)
-        exit()
-    else:
-        x += 1
-        prev = cur
-        cur = set()
-
-
-#OR.. THIS IS NO GOOD
-
-
-# for each layer....x
-# we determine the # of coins in the LAST group, l
-# then look up n(x-l), and sum that in.
-# n(0) = 1, 1 way to order no coins
-#n = [1]
-
-#def coin_partitions(x):
-#    total = 0
-#    for l in range(1,x+1): # number of coins in last group
-#        total += n(x-l) # number of ways to pre-order that
-        # no... this is not unique
-
-
+# For example, if we have a row of size 7, and we pick a final
+# group of size 2:
+# Then we look at entry (5,2) to determine how many ways we can order
+# an array of length 5 with last entry at least length 2
