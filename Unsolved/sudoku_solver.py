3ceba5954e5ab591c1bdd4bdedcf5db26d25ee39
Nathan Andersen <nandersen@cimpress.com>
2016-06-15 16:17:21 -0400

    Slowly moving forward.

--- Unsolved/sudoku_solver.py
+++ Unsolved/sudoku_solver.py
@@ -1,26 +1,18 @@
 # Problem 96
 # Solving Sudoku
 
-# We will implement this with a queue
-from queue import Queue
-
 digits = {d for d in range(1,10)}
 sudokuLen = 9
-
-
-# Elementary algorithm:
-
-# Add all squares in grid to the queue.
-# While queue is not empty:
-    # If the square is unsolved:
-        #add it to the back of the queue
-    # Else:
-        #then apply the Sudoku Rules
-        # to eliminate its number from row/column/section.
-        # Do not re-add to queue.
-
 # A Sudoku puzzle is a 9x9 list.
-
+def prettyPrint(puzzle):
+    for col in range(sudokuLen):
+        for row in range(sudokuLen):
+            if isinstance(puzzle[col][row],int):
+                print(puzzle[col][row],end=" ")
+            else:
+                print(".",end=" ")
+        print()
+    print()
 def createSolutionMatrix(p):
     # Pre-process
     s = p.copy()
@@ -30,7 +22,6 @@ def createSolutionMatrix(p):
                 s[y][x] = [d for d in digits]
             else:
                 s[y][x] = int(p[y][x])
-
     return s
 def prepareQueue():
     # Return a queue with all squares in the sudoku matrix
@@ -48,20 +39,15 @@ def prepareSet(p):
         for y in range(sudokuLen):
             if isinstance(p[y][x],int):
                 s.add((x,y))
-
     return s
-
 def removeValFromSquarePossibilities(x,y,v,solution,gridSet):
     if isinstance(solution[y][x],int): return
-
     try:
         solution[y][x].remove(v)
         if len(solution[y][x]) == 1:
             solution[y][x] = solution[y][x][0]
             gridSet.add((x,y))
-    except ValueError:
-        pass
-
+    except ValueError: pass
 def checkForUniqueValuesInRow(y,solution,gridSet):
     for d in digits:
         numOccurrences = 0
@@ -145,16 +131,61 @@ def eliminatePossibilities(x,y,solution,gridSet):
     checkForUniqueValuesInRow(y,solution,gridSet)
     checkForUniqueValuesInColumn(x,solution,gridSet)
     checkForUniqueValuesInSection(x,y,solution,gridSet)
+def isSolved(p):
+    for x in range(sudokuLen):
+        for y in range(sudokuLen):
+            if not isinstance(p[y][x],int):
+                return False
+    return True
 
-def prettyPrint(puzzle):
+def smartCheckSectionsByRow(p,gridSet):
+    # If a digit is missing in a section, and is only
+    # available in a certain row, then remove it from
+    # the rest of the section
+    for d in digits:
+        pass
+def smartCheckSectionsByColumn(p,gridSet):
+    pass
+
+
+def findOccurrencesInRow(d,p,row):
+    occurrences = []
     for col in range(sudokuLen):
-        for row in range(sudokuLen):
-            if isinstance(puzzle[col][row],int):
-                print(puzzle[col][row],end=" ")
-            else:
-                print(".",end=" ")
-        print()
-    print()
+        if isinstance(p[row][col],int):
+            if p[row][col] == d:
+                return None
+        elif d in p[row][col]:
+            occurrences.append((col,row))
+    return occurrences
+def smartCheckRowsBySection(p,gridSet):
+        # If a digit is missing in a row,
+        # and is only available in a certain section
+        #, then remove it from the rest of the row
+
+    for d in digits:
+        for sect in range(sudokuLen):
+            rowOccurrences = findOccurrencesInRow(d,p,sect)
+            sectOccurrences = findOccurrencesInSection(d,p,sect)
+            # If those occurrences
+            # are the only ones in the section
+
+
+    pass
+def smartCheckColumnsBySection(p,gridSet):
+    pass
+
+def smartCheck(p,gridSet):
+    # If a section is lacking value v, and v is only possible
+    # in a row or a column, than we can remove v from that
+    # row and column in other sections
+
+    # Similarly, if a row or column is lacking value v, and v is only
+    # possible in a given section, then we can remove v from other entries
+    # in that section.
+    smartCheckSectionsByRow(p,gridSet)
+    smartCheckSectionsByColumn(p,gridSet)
+    smartCheckRowsBySection(p,gridSet)
+    smartCheckColumnsBySection(p,gridSet)
 
 def solveSudoku(p):
     solution = createSolutionMatrix(p)
@@ -165,23 +196,35 @@ def solveSudoku(p):
             (x,y) = gridSet.pop()
             eliminatePossibilities(x,y,solution,gridSet)
         except KeyError:
-            prettyPrint(solution)
-            return
+
+            if isSolved(solution):
+                prettyPrint(solution)
+                return solution[0][0]*100 + solution[0][1]*10 + solution[0][2]
+            else:
+                smartCheck(solution,gridSet)
+                # Do some harder cross-checking here.
+
+                prettyPrint(solution)
+                return 0
 
 
 
 if __name__ == "__main__":
     puzzles = []
     curPuzzle = []
+    topLeftSum = 0
     f = open("p096_sudoku.txt")
     f.readline()
     for line in f:
         if line.startswith("Grid"):
             puzzles.append(curPuzzle)
+            topLeftSum += solveSudoku(curPuzzle)
             curPuzzle = []
         else:
             curPuzzle.append(list(line.strip()))
 
-    for p in puzzles:
-        solveSudoku(p)
+    print(topLeftSum)
+#    for p in puzzles:
+#        topLeftSum += solveSudoku(p)
+#        print(solveSudoku(p))
 #    solveSudoku(puzzles[3])
