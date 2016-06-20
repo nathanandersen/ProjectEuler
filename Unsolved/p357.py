08afe8aebf33b0363da46197b687da5754686095
Nathan Andersen <nandersen@cimpress.com>
2016-06-20 16:18:03 -0400

    Thinking about 357.

--- Unsolved/p357.py
+++ Unsolved/p357.py
@@ -6,3 +6,11 @@
 # 100 million cannot be brute-forced.
 # so these numbers must have some special property.
 
+if gcd(k/f,f) is not 1, then false
+
+Therefore the only numbers to consider are ones that are made up
+of combinations of primes
+
+30 = 2 * 3 * 5
+not 60
+42 = 2 * 3 * 7
