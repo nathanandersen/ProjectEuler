7ee1e152760caabc694b935cf32fc3f4dfa913f6
Nathan Andersen <nathan.andersens@gmail.com>
2017-03-09 14:46:47 +0100

    updated 115

--- Solved/p115.py
+++ Solved/p115.py
@@ -1,24 +1,18 @@
-# Problem 115
+4# Problem 115
 
 #real	0m0.046s
 #user	0m0.028s
 #sys	0m0.009s
 
-n = [1]
+n = {0:1}
 m = 50
 def tile_count(x):
-    total = n[x-1]
-    for i in range(m,x+1):
-        if i is x: total += 1
-        else: total += n[x-i-1]
-    return total
+    return n[x-1] + sum(1 if i == x else n[x-i-1] for i in range(m,x+1))
 
-x = 1
-while True:
-    tc = tile_count(x)
-    if tc > 1000000:
-        print(x,tc)
-        exit()
-    else:
-        n.append(tc)
-        x += 1
+counter,tc = 0,0
+while tc < 1000000:
+    counter += 1
+    tc = tile_count(counter)
+    n[counter] = tc
+
+print(counter,tc)
